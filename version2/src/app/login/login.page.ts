b8b39361405508c75a4a0528ea346e4d48dae05b
Aryan Sharma <aryan1997.17@gmail.com>
2020-07-07 05:18:53 +0530

    made intro page as the startup page

--- version2/src/app/login/login.page.ts
+++ version2/src/app/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+
 
 
 @Component({
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor( private router: Router) { }
+  constructor() { }
 
   ngOnInit() {
   }
